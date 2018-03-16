af2ddf5cb2f8cfbe61429b372c76eee7657c8aa7
nevermore6264 <37317921+nevermore6264@users.noreply.github.com>
2018-03-16 16:21:14 +0700

    [Bài tập] Ứng dụng xổ số

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+        int[] a = {4,7,2,6};
+        System.out.println(a.length);
+        System.out.println(a[2]*2);
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+    }
+}
